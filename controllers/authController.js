32ba096bbd904c94b135e0950ae9fe8399ad7800
SurajMMH <suraj@mmh.homes>
2024-08-11 04:19:24 +0530

    Fix login API

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,28 +7,43 @@ module.exports = {
     try {
       const user = new User(req.body);
       const data = await user.save();
-      const token = jwt.sign(
-      { id: user._id, username: user.username, email: user.email, isAdmin: user.isAdmin },
-      Secret.jwt_secret
-    );
-      res.status(201).send({jwt_token:token,  message: "User created successfully" });
+      res.status(200).send({ message: "User created successfully" });
     } catch (err) {
       res.status(400).send({ message: "Error creating user" });
     }
   },
 
   login: async (req, res) => {
+    const { email, password } = req.body;
+
     try {
-      const user = await User.findOne({ email: req.body.email });
-      if (!user || !user.isApproved) {
-        return res.status(401).send({ message: "Unauthorized" });
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(401).send({ message: "Invalid credentials" });
       }
-      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-        expiresIn: "1h",
-      });
-      res.send({ token });
+
+      // if (!user.isApproved) {
+      //   return res.status(403).send({ message: "User is not approved" });
+      // }
+
+      const isMatch = await user.matchPassword(password);
+      if (!isMatch) {
+        return res.status(401).send({ message: "Invalid credentials" });
+      }
+
+      const token = jwt.sign(
+        {
+          id: user._id,
+          username: user.username,
+          email: user.email,
+          isAdmin: user.isAdmin,
+        },
+        Secret.jwt_secret
+      );
+      res.status(200).send({ token });
     } catch (err) {
-      res.status(401).send({ message: "Invalid credentials" });
+      console.error(err);
+      res.status(500).send({ message: "Server error" });
     }
   },
 };
