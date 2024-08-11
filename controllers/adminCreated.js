e0ec3332c9718b786fb9eec5f23bab1ea21ae89b
SurajMMH <suraj@mmh.homes>
2024-08-12 02:10:51 +0530

    fix issue

--- controllers/adminCreated.js
+++ controllers/adminCreated.js
@@ -1,25 +1,28 @@
 const Admin = require("../models/User");
 
-const adminPayload = {
-  username: "Suraj97",
-  email: "surajkarosia97@gmail.com",
-  password: "Suraj@9926858505",
-  isAdmin: true,
-};
+const usernam = "Suraj96";
+const email = "surajkarosia96@gmail.com";
+const password = "Suraj@9926858505";
+const adminStatus = true;
+const isApprove = "Approved";
 
 const register = async (req, res) => {
   try {
-    const { username, email } = adminPayload;
-
     const existingAdmin = await Admin.findOne({
-      $or: [{ username: username }, { email: email }],
+      $or: [{ username: usernam }, { email: email }],
     });
 
     if (existingAdmin) {
       console.log("Admin with this username or email already exists.");
       return;
     }
-    const admin = new Admin(adminPayload);
+    const admin = new Admin({
+      username: usernam,
+      email,
+      password,
+      isAdmin: adminStatus,
+      isApproved: isApprove,
+    });
     await admin.save();
   } catch (err) {
     console.log(err);
