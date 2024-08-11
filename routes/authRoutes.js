c11c8e0fd733090b73d2837c46ae9c6b727f4bec
SurajMMH <suraj@mmh.homes>
2024-08-12 00:28:58 +0530

    fix auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/authController");
-const AuthMiddleware = require("../middlewares/auth");
 
 router.post("/register", register);
-router.use(AuthMiddleware.protect);
 router.post("/login", login);
 
 module.exports = router;
